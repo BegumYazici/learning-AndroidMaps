a84b93e14cb3cdfbd2a11ddb9b08f1e6fadd8c0c
Begum.Yazici <yazicibegum@gmail.com>
2017-11-16 15:06:01 +0200

    Andrıoid Maps application is make and commit.

--- app/AndroidMaps/app/src/main/java/maps/begi/com/androidmaps/MainActivity.java
+++ app/AndroidMaps/app/src/main/java/maps/begi/com/androidmaps/MainActivity.java
@@ -1,13 +1,36 @@
 package maps.begi.com.androidmaps;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity {
+
+    private GoogleMap googleHarita;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (googleHarita == null) {
+            googleHarita = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.haritafragment))
+                    .getMap();
+            if (googleHarita != null) {
+                //İstanbul, Kız Kulesi olsun.
+                LatLng istanbulKoordinat = new LatLng(41.021161, 29.004065);
+                googleHarita.addMarker(new MarkerOptions().position(istanbulKoordinat).title("Kız Kulesi"));  //harita imleci
+                googleHarita.moveCamera(CameraUpdateFactory.newLatLngZoom(istanbulKoordinat, 13));
+                googleHarita.setMapType(GoogleMap.MAP_TYPE_SATELLITE);  //uydu görünümü
+                googleHarita.setMyLocationEnabled(true); //kullanıcı konumunu da gösterir.
+            }
+        }
+
     }
 }
